69bd8dd469b18c25ae6a3951b02c5193f4f223b1
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-01-30 15:46:27 -0600

    If else statements mod 3

---
+++ module_3/3.2.2_Basic_if-else.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TicketCounter {
+   public static void main (String [] args) {
+      int awardPoints;
+      int userTickets;
+   
+      Scanner scnr = new Scanner(System.in);
+      userTickets = scnr.nextInt(); // Program will be tested with values: 5, 6, 7, 8.
+   
+      if (userTickets == 7) {
+         awardPoints = 1;
+      } else {
+         awardPoints = userTickets;
+      }
+   
+      System.out.println(awardPoints);
+   }
+}
---
+++ module_3/3.2.2_Basic_if-else_lv2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberDifference {
+   public static void main (String [] args) {
+      int totalDifference;
+      int numDifference;
+   
+      Scanner scnr = new Scanner(System.in);
+      numDifference = scnr.nextInt(); // Program will be tested with values: -14, -15, -16, -17.
+      
+      if (numDifference == -15) {
+         totalDifference = -25;
+      } else {
+         totalDifference = numDifference;
+      }
+      
+      System.out.println(totalDifference);
+   }
+}

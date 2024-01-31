6c4c6ce678452a1222cf6808dbab8a8fa77dca99
WickedYoda <68271168+wickedyoda@users.noreply.github.com>
2024-01-31 12:29:38 -0600

    3.6 compelte

---
+++ module_3/LogicalTest.java
@@ -0,0 +1,15 @@
+
+public class LogicalTest {
+    public static void main (String [] args) {
+       int x;
+ 
+       x = 5;
+       
+       if ( (x < 5) && (x > 2) ) {
+          System.out.println("a");
+       }
+       else {
+          System.out.println("b");
+       }
+    }
+ }
---
+++ test_files/test1.java
@@ -0,0 +1,9 @@
+if ((userChannel >= 2) && (userChannel <= 499)) {
+    channelType = 's';
+ }
+ else if ((userChannel >= 1002) && (userChannel <= 1499)) {
+    channelType = 'h';
+ }
+ else {
+    channelType = 'e';
+ }
